8c30236020bab295a951c5df0bafb389903b142b
Dallin Major <dallin.watac.major@gmail.com>
2019-08-09 14:14:29 -0600

    got some good stuff changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import socketIO from 'socket.io-client';
+import './App.css';
 
 const socket = socketIO('http://localhost:5000/', {
   transports: ['websocket'],
@@ -14,6 +15,10 @@ socket.on('connect', () => {
 
 const App = () => {
   const [messages, setMessages] = useState([]);
+  const [chatroomId, setChatroomId] = useState();
+  const [senderId, setSenderId] = useState();
+  const [recieverId, setRecieverId] = useState();
+  const [socketListening, setSocketListening] = useState(false);
 
   const chatRoom = 1;
 
@@ -27,6 +32,7 @@ const App = () => {
       socket.removeAllListeners('privateMessage');
     };
   }, [messages]);
+
 
   const handleSubmit = event => {
     const body = event.target.value;
@@ -48,6 +54,33 @@ const App = () => {
     }
   };
 
+  function listenForMessages() {
+    socket.on('private_message_' + recieverId, message => {
+      console.log(message);
+      setMessages([message, ...messages]);
+    });
+    setSocketListening(true);
+  }
+
+  function handleChatroomInput(e) {
+    const value = e.target.value;
+    setChatroomId(value);
+  }
+
+  function handleSenderIdInput(e) {
+    const value = e.target.value;
+    setSenderId(value);
+  }
+
+  function handleRecieverIdInput(e) {
+    const value = e.target.value;
+    if (e.keyCode === 13 && value) {
+      listenForMessages();
+    } else {
+      setRecieverId(value);
+    }
+  }
+
   const messageList = messages.map((message, index) => {
     return (
       <li key={index}>
@@ -57,15 +90,43 @@ const App = () => {
   });
 
   return (
-    <>
+    <div className='container'>
       <h1>Hello World!</h1>
+      <div>Enter Chatroom Id</div>
+      <input
+        type="text"
+        placeholder="Chatroom"
+        value={chatroomId}
+        onChange={handleChatroomInput}
+      />
+      <br/><br/>
+      <div>Socket {socketListening ? 'ON' : 'OFF'}</div>
+     <div>give Agent Id to initialize Socket</div>
+      <input
+        type="text"
+        placeholder="Enter Id"
+        value={recieverId}
+        onKeyUp={handleRecieverIdInput}
+      />
+      <br/><br/>
+     <div>Agent id</div>
+      <input
+        type="text"
+        placeholder="Enter Id"
+        value={senderId}
+        onKeyUp={handleSenderIdInput}
+      />
+      <br/><br/>
       <input
         type="text"
         placeholder="Enter a message..."
         onKeyUp={handleSubmit}
       />
+      <br/>
+      <br/>
+      <br/>
       {messageList}
-    </>
+    </div>
   );
 };
 
